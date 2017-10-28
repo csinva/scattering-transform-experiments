12979c2f45bea90b9242d8ff364f7cef0b7bc2b1
Chris Lu <luchris429@gmail.com>
2017-10-28 00:30:16 -0700

    added visualizer

--- pytorch-classification/kernel_visualizer.py
+++ pytorch-classification/kernel_visualizer.py
@@ -7,7 +7,7 @@ import torchvision.models as models
 import numpy as np
 from matplotlib import pyplot as plt
 
-def plot_kernels(tensor, num_cols=22):
+def plot_kernels(tensor, num_cols=8):
     if not tensor.ndim==4:
         raise Exception("assumes a 4D tensor")
     if not tensor.shape[-1]==3:
@@ -17,7 +17,7 @@ def plot_kernels(tensor, num_cols=22):
     fig = plt.figure(figsize=(num_cols,num_rows))
     for i in range(tensor.shape[0]):
         ax1 = fig.add_subplot(num_rows,num_cols,i+1)
-        minned = tensor[i] + np.min(tensor[i])
+        minned = tensor[i] - np.min(tensor[i])
         ax1.imshow(minned/np.max(minned))
         ax1.axis('off')
         ax1.set_xticklabels([])
@@ -31,10 +31,26 @@ best_alexnet = torch.load('model_best.pth.tar')
 conv1_weights = best_alexnet['state_dict']['module.features.0.weight'].cpu().numpy()
 tensor = np.swapaxes(conv1_weights,1,3)
 
-for i in range(tensor.shape[0]):
-    minned = tensor[i] + np.min(tensor[i])
-    plt.imshow(minned/np.max(minned))
-    plt.show()
+plot_kernels(tensor)
+# print("SHOWING NORAML ALEXNET")
+# for i in range(tensor.shape[0]):
+#     minned = tensor[i] - np.min(tensor[i])
+#     norm = minned/np.max(minned)
+#     plt.imshow(norm)
+#     plt.show()
+
+alexscat = torch.load('checkpoints/cifar100/alexscat/model_best.pth.tar')
+conv1_weights = alexscat['state_dict']['module.first_layer.0.weight'].cpu().numpy()
+tensor = np.swapaxes(conv1_weights,1,3)
+plot_kernels(tensor)
+
+# print("NOW SHOWING SCATTERING VERSION")
+
+# for i in range(tensor.shape[0]):
+#     minned = tensor[i] - np.min(tensor[i])
+#     norm = minned/np.max(minned)
+#     plt.imshow(norm)
+#     plt.show()
 
 #vis = Visdom()
 #vis.image(conv1_weights[0])
